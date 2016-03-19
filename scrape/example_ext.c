e852c1c9325a105521eccdbe2b129cd1e1ca051d
yorickdewid <yorick17@outlook.com>
2016-03-19 17:30:24 +0100

    Move scraper

---
+++ scrape/example_ext.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+
+char *mav_main(int argc, char *argv[]) {
+	struct passwd *p = getpwuid(getuid());
+	if (!p)
+		return NULL;
+
+	printf("Current user: %s\n", p->pw_name);
+
+	/* Return some data */
+	return p->pw_name;
+}
