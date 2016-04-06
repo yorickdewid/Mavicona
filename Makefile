edea530acfe9918a0a6c73da0e9e7a8948076a73
yorickdewid <yorick17@outlook.com>
2016-04-06 19:28:14 +0200

    Let cynder listen on 5588

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ all:
 	$(MAKE) -C scrape
 	$(MAKE) -C extract
 	$(MAKE) -C pitch
+	$(MAKE) -C cynder
 
 clean:
 	$(MAKE) -C common clean
@@ -13,3 +14,4 @@ clean:
 	$(MAKE) -C scrape clean
 	$(MAKE) -C extract clean
 	$(MAKE) -C pitch clean
+	$(MAKE) -C cynder clean
--- cynder/cynder.cpp
+++ cynder/cynder.cpp
@@ -2,7 +2,40 @@
 #include <string>
 #include <iostream>
 
+#include <ups/upscaledb.hpp>
+
 int main(int argc, char *argv[]) {
+
+	// GOOGLE_PROTOBUF_VERIFY_VERSION;
+	upscaledb::env env;
+
+	/* Prepare our context and socket */
+	zmq::context_t context(1);
+	zmq::socket_t socket(context, ZMQ_REP);
+
+	int opt = 1;
+	socket.setsockopt(ZMQ_IPV6, &opt, sizeof(int));
+	socket.bind("tcp://*:5588");
+
+	std::cout << "Waiting for connections " << std::endl;
+
+	while (true) {
+		zmq::message_t request;
+
+		/* Wait for next request from client */
+		socket.recv(&request);
+
+		// Task task;
+		// task.ParseFromArray(request.data(), request.size());
+
+		/* Handle incomming task */
+		// parseTask(task);
+
+		/* Send reply back to client */
+		zmq::message_t reply(7);
+		memcpy(reply.data(), "STORED", 7);
+		socket.send(reply);
+	}
 
 	return 0;
 }
