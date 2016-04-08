29bb42fb37895a0740296cabce5d9adbff1c8793
yorickdewid <yorick17@outlook.com>
2016-04-08 16:16:02 +0200

    Consistent hashing for database sharding

---
+++ cynder/consistent_hash.h
@@ -0,0 +1,84 @@
+#ifndef CONSISTENT_HASH_H
+#define CONSISTENT_HASH_H
+
+#include <map>
+#include <string>
+#include <sstream>
+
+namespace {
+
+class StringifyException {};
+
+template <class T>
+std::string Stringify(const T& t) {
+	std::ostringstream os;
+	if (!(os << t)) {
+		throw StringifyException();
+	}
+	return os.str();
+}
+
+template <>
+std::string Stringify(const std::string& str) {
+	return str;
+}
+
+template <>
+std::string Stringify(const char* const& str) {
+	return str;
+}
+
+}
+
+namespace Consistent {
+
+class EmptyRingException {};
+
+template <class Node, class Key, class Hash = std::hash<const char *>>
+class HashRing {
+	std::map<size_t, Node> ring_;
+	const unsigned int m_spread;
+	Hash hash_;
+
+  public:
+	HashRing(unsigned int spread) : m_spread(spread), hash_(std::hash<const char*>()) {}
+	HashRing(unsigned int spread, const Hash& hash) : m_spread(spread), hash_(hash) {}
+
+	size_t addNode(const Node& node) {
+		size_t hash;
+		std::string nodestr = Stringify(node);
+		for (unsigned int r = 0; r < m_spread; r++) {
+			hash = hash_((nodestr + Stringify(r)).c_str());
+			ring_[hash] = node;
+		}
+		return hash;
+	}
+
+	void removeNode(const Node& node) {
+		std::string nodestr = Stringify(node);
+		for (unsigned int r = 0; r < m_spread; r++) {
+			size_t hash = hash_((nodestr + Stringify(r)).c_str());
+			ring_.erase(hash);
+		}
+	}
+
+	const Node& getNode(const Key& key) const {
+		if (ring_.empty()) {
+			throw EmptyRingException();
+		}
+		size_t hash = hash_(Stringify(key).c_str());
+
+		// Look for the first node >= hash
+		typename std::map<size_t, Node>::const_iterator it = ring_.lower_bound(hash);
+		if (it == ring_.end()) {
+			// Wrapped around; get the first node
+			it = ring_.begin();
+		}
+
+		return it->second;
+	}
+};
+
+}
+
+#endif // CONSISTENT_HASH_H
---
+++ cynder/server_node.h
@@ -0,0 +1,48 @@
+#ifndef SERVER_NODE_H
+#define SERVER_NODE_H
+
+#include <map>
+#include <string>
+
+class ServerNode {
+	std::string m_address;
+	std::string m_name;
+	std::map<std::string, std::string> m_cache;
+
+  public:
+	ServerNode() {}
+	ServerNode(std::string address) : m_address(address) {}
+	ServerNode(std::string address, std::string name) : m_address(address), m_name(name) {}
+
+	std::string info() const {
+		if (m_address.empty())
+			return "unknown";
+
+		if (m_name.empty())
+			return m_address;
+
+		return m_address + " (" + m_name + ")";
+	}
+
+	void put(const std::string& key, const std::string& value) {
+		m_cache[key] = value;
+	}
+
+	std::string get(const std::string& key) const {
+		std::string value;
+		std::map<std::string, std::string>::const_iterator it = m_cache.find(key);
+		if (it != m_cache.end()) {
+			value = it->second;
+		}
+		return value;
+	}
+
+	void remove(const std::string& key) {
+		std::map<std::string, std::string>::iterator it = m_cache.find(key);
+		if (it != m_cache.end()) {
+			m_cache.erase(it);
+		}
+	}
+};
+
+#endif // SERVER_NODE_H
