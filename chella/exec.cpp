dceec708c79beb2545d5dec6c2528bbfb3150c7e
yorickdewid <yorick17@outlook.com>
2016-06-29 20:52:07 +0200

    Exec class

---
+++ chella/exec.cpp
@@ -0,0 +1,5 @@
+#include "exec.h"
+
+void Execute::run(const std::string& name) {
+	std::cout << "Running module " << name << std::endl;
+}
---
+++ chella/exec.h
@@ -0,0 +1,13 @@
+#ifndef EXEC_H
+#define EXEC_H
+
+#include <iostream>
+
+class Execute {
+  public:
+	Execute() {}
+
+	static void run(const std::string& name);
+};
+
+#endif // EXEC_H
