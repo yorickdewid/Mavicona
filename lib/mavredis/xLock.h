d3b42b7781de3b635e1810f4c6f002cfe003a334
yorickdewid <yorick17@outlook.com>
2016-07-12 23:48:46 +0200

    Makefile
    - Style

--- lib/mavredis/xLock.h
+++ lib/mavredis/xLock.h
@@ -1,11 +1,9 @@
-/*
- * ----------------------------------------------------------------------------
- * Copyright (c) 2012-2013, xSky <guozhw at gmail dot com>
+/**
+ * Copyright (c) 2016 Mavicona
  * All rights reserved.
- * Distributed under GPL license.
- * ----------------------------------------------------------------------------
+ *
+ * Mavicona Redis Client
  */
-
 
 #ifndef _XLOCK_H_
 #define _XLOCK_H_
@@ -20,63 +18,63 @@
 
 
 class xLock {
-private:
+  private:
 #ifdef WIN32
-    CRITICAL_SECTION    mSection;
+	CRITICAL_SECTION    mSection;
 #else
-    pthread_mutex_t     mMutex;
+	pthread_mutex_t     mMutex;
 #endif
-    
-public:
-    inline xLock() {
+
+  public:
+	inline xLock() {
 #ifdef WIN32
-        InitializeCriticalSection(&mSection);
+		InitializeCriticalSection(&mSection);
 #else
-        pthread_mutexattr_t attr;
-        pthread_mutexattr_init(&attr);
-        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-        int ret = pthread_mutex_init(&mMutex, &attr);
-        if(ret != 0 ){
-            fprintf(stderr,"pthread_mutex_init error %d \n\r",ret);
-        }
+		pthread_mutexattr_t attr;
+		pthread_mutexattr_init(&attr);
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+		int ret = pthread_mutex_init(&mMutex, &attr);
+		if (ret != 0 ) {
+			fprintf(stderr, "pthread_mutex_init error %d \n\r", ret);
+		}
 #endif
-    };
-    inline ~xLock() {
+	};
+	inline ~xLock() {
 #ifdef WIN32
-        DeleteCriticalSection(&mSection);
+		DeleteCriticalSection(&mSection);
 #else
-        pthread_mutex_destroy(&mMutex);
+		pthread_mutex_destroy(&mMutex);
 #endif
-    }
-    inline void  Enter() {
+	}
+	inline void  Enter() {
 #ifdef WIN32
-        EnterCriticalSection(&mSection);
+		EnterCriticalSection(&mSection);
 #else
-        pthread_mutex_lock(&mMutex);
+		pthread_mutex_lock(&mMutex);
 #endif
-    }
-    inline void Leave() {
+	}
+	inline void Leave() {
 #ifdef WIN32
-        LeaveCriticalSection(&mSection);
+		LeaveCriticalSection(&mSection);
 #else
-        pthread_mutex_unlock(&mMutex);
+		pthread_mutex_unlock(&mMutex);
 #endif
-    };
+	};
 };
 
 class CLockUser {
-public:
-    inline  CLockUser(xLock& lock):mlock(lock) {
-        mlock.Enter();
-    };    
-    inline  ~CLockUser(){
-        mlock.Leave();
-    }
-private:
-    xLock& mlock;
+  public:
+	inline  CLockUser(xLock& lock): mlock(lock) {
+		mlock.Enter();
+	};
+	inline  ~CLockUser() {
+		mlock.Leave();
+	}
+  private:
+	xLock& mlock;
 };
 
 #define XLOCK(T) CLockUser lock(T)
 
-#endif 
+#endif
 
--- lib/mavredis/xRedisClient_connection.cpp
+++ lib/mavredis/xRedisClient_connection.cpp
@@ -1,26 +1,24 @@
-/*
- * ----------------------------------------------------------------------------
- * Copyright (c) 2013-2014, xSky <guozhw at gmail dot com>
+/**
+ * Copyright (c) 2016 Mavicona
  * All rights reserved.
- * Distributed under GPL license.
- * ----------------------------------------------------------------------------
+ *
+ * Mavicona Redis Client
  */
 
 #include "xRedisClient.h"
 #include <sstream>
 
-void xRedisClient::quit(){
+void xRedisClient::quit() {
 	Release();
 }
 
 
-bool xRedisClient::echo(const RedisDBIdx& dbi, const string& str, std::string &value)
-{
-	if (0==str.length()) {
-        return false;
-    }
-    SETDEFAULTIOTYPE(MASTER);
-    return command_string(dbi, value, "echo %s", str.c_str());
+bool xRedisClient::echo(const RedisDBIdx& dbi, const string& str, std::string& value) {
+	if (0 == str.length()) {
+		return false;
+	}
+	SETDEFAULTIOTYPE(MASTER);
+	return command_string(dbi, value, "echo %s", str.c_str());
 }
 
 
