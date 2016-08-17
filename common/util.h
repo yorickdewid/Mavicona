42f7cb2e7e295fabeb32998e3eb2121628ff87d6
yorickdewid <yorick17@outlook.com>
2016-08-17 22:45:56 +0200

    Directory helper

--- common/util.h
+++ common/util.h
@@ -10,6 +10,11 @@ inline bool file_exist(const std::string& name) {
 	return (stat(name.c_str(), &buffer) == 0);
 }
 
+inline bool dir_exist(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0 && S_ISDIR(buffer.st_mode));
+}
+
 inline std::string normalize(std::string& str) {
 	std::transform(str.begin(), str.end(), str.begin(), [](char c) {
 		return tolower(c);
