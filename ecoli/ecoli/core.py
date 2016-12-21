1ae6da17489f0f25fc96f8813a86e6a0a3e504a3
yorickdewid <yorick17@outlook.com>
2016-12-21 16:22:19 +0100

    Update to requests

--- ecoli/ecoli/core.py
+++ ecoli/ecoli/core.py
@@ -1,5 +1,22 @@
+##
+# Copyright (C) 2015-2016 Mavicona, Quenza Inc.
+# All Rights Reserved
+#
+# This file is part of the Mavicona project.
+#
+# Content can not be copied and/or distributed without the express
+# permission of the author.
+#
+# TODO: Download job files, not zip
+#
+
 import os
 import sys
+import time
+import string
+import random
+import requests
+import datetime
 import argparse
 import shutil
 import simplejson
@@ -12,7 +29,8 @@ default_url = 'http://ecoli.mavicona.net/api/v1/repository'
 file_name = 'index.json'
 
 def query_yes_no(question, default="yes"):
-	"""Ask a yes/no question via raw_input() and return their answer.
+	"""
+	Ask a yes/no question via raw_input() and return their answer.
 
 	"question" is a string that is presented to the user.
 	"default" is the presumed answer if the user just hits <Enter>.
@@ -45,11 +63,17 @@ def signal_handler(signal, frame):
 	print()
 	sys.exit(0)
 
+def get_local_dir():
+	return os.path.expanduser("~") + '/.ecoli'
+
+def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
 def package_get_location(search):
-	if not os.path.isfile('.ecoli/' + file_name):
+	if not os.path.isfile(get_local_dir() + '/' + file_name):
 		update_repo()
 
-	with open('.ecoli/' + file_name) as repo:
+	with open(get_local_dir() + '/' + file_name) as repo:
 		data = simplejson.load(repo)
 		for package in data['packages']:
 			if search not in package['name'].lower():
@@ -60,8 +84,8 @@ def package_get_location(search):
 	return None
 
 def install(install_list, db):
+	""" Install package """
 	print('Installing')
-
 	downloader = []
 	for package in install_list:
 		loc = package_get_location(package)
@@ -77,7 +101,7 @@ def install(install_list, db):
 		print('Downloading...')
 		for download in downloader:
 			reponse = urllib.request.urlopen(download['location'])
-			file = open('.ecoli/tmp/' + download['name'].lower() + '.zip', 'wb')
+			file = open(get_local_dir() + '/tmp/' + download['name'].lower() + '.zip', 'wb')
 			print('Fetching %s...' % download['name'], end="")
 
 			block_sz = 8192
@@ -91,52 +115,70 @@ def install(install_list, db):
 
 			file.close()
 
-			with zipfile.ZipFile('.ecoli/tmp/' + download['name'].lower() + '.zip', 'r') as czip:
-				czip.extractall('.ecoli/packages/' + download['name'])
+			with zipfile.ZipFile(get_local_dir() + '/tmp/' + download['name'].lower() + '.zip', 'r') as czip:
+				czip.extractall(get_local_dir() + '/packages/' + download['name'])
 
 			db.ladd('packages_installed', download)
 
 			print('[done]')
 
 def remove(remove_list, db):
+	""" Remove local package """
 	for package in remove_list:
 		for index,install in enumerate(db.lgetall('packages_installed')):
 			if package.lower() == install['name'].lower():
 				print('Removing ' + install['name'])
 
-				shutil.rmtree('.ecoli/packages/' + install['name'])
+				shutil.rmtree(get_local_dir() + '/packages/' + install['name'])
 				db.lpop('packages_installed', index)
 
-#TODO: traverse additional repsitories
 def update_repo(db):
-	print('Updating catalogs...', end="")
+	""" Fetch remote package catalog """
+	if db.get('last_updated'):
+		print('Last update:',datetime.datetime.fromtimestamp(db.get('last_updated'))
+			.strftime('%Y-%m-%d %H:%M:%S'))
 
+	pkgdb = pickledb.load(get_local_dir() + '/index.db', True)
+	pkgdb.lcreate('package')
 	for source in db.lgetall('sources'):
-		reponse = urllib.request.urlopen(source)
-		file = open('.ecoli/' + file_name, 'wb')
+		print('Updating catalog...', end="")
+		try:
+			data = requests.get(source).json()
+		except simplejson.scanner.JSONDecodeError as q:
+			print('[invalid]')
+			print(q)
+			continue
 
-		block_sz = 8192
-		while True:
-			buffer = reponse.read(block_sz)
-			if not buffer:
-				break
+		except requests.exceptions.ConnectionError:
+			print('[error]')
+			continue
 
-			file.write(buffer)
-			print('.', end="")
+		if data['meta']['repository'] != 'main':
+			print('[skip]')
+			continue
 
-		file.close()
+		for package in data['packages']:
+			pkgdb.ladd('package', package)
 
-	print('[done]')
+		for repo in data['repositories']:
+			db.ladd('sources', repo)
+
+		print('[done]')
+
+	db.set('last_updated', time.time())
 
 def verify_datadir():
-	if not os.path.exists('.ecoli'):
-		os.makedirs('.ecoli')
-		os.makedirs('.ecoli/data')
-		os.makedirs('.ecoli/packages')
-		os.makedirs('.ecoli/tmp')
+	""" Check all directories exist """
+
+	if not os.path.exists(get_local_dir()):
+		os.makedirs(get_local_dir())
+		os.makedirs(get_local_dir() + '/data')
+		os.makedirs(get_local_dir() + '/packages')
+		os.makedirs(get_local_dir() + '/tmp')
 
 def update_packages(db):
-	if not os.path.isfile('.ecoli/' + file_name):
+	""" Update all local packages """
+	if not os.path.isfile(get_local_dir() + '/index.db'):
 		update_repo(db)
 
 	for index,local in enumerate(db.lgetall('packages_installed')):
@@ -154,34 +196,34 @@ def update_packages(db):
 				install([package['name'].lower()], db)
 
 def list_repo(db, search=""):
-	if not os.path.isfile('.ecoli/' + file_name):
+	""" List repositories """
+	if not os.path.isfile(get_local_dir() + '/index.db'):
 		update_repo(db)
 
-	with open('.ecoli/' + file_name) as repo:
-		data = simplejson.load(repo)
-		for package in data['packages']:
-			if search:
-				if search not in package['name'].lower() and search not in package['description'].lower():
-					continue
-			
-			print("{}\t\t{}".format(package['name'], package['description']))
+	pkgdb = pickledb.load(get_local_dir() + '/index.db', True)
+	for package in pkgdb.lgetall('package'):
+		if search:
+			if search not in package['name'].lower() and search not in package['description'].lower():
+				continue
+		
+		print("{}\t\t{}".format(package['name'], package['description']))
 
 def list_sources(db):
-	for source in db.lgetall('sources'):			
+	for source in db.lgetall('sources'):
 		print(source)
 
+def add_sources(sources, db):
+	for source in sources:
+		db.ladd('sources', source)
+
 def cleanup():
-	shutil.rmtree('.ecoli/tmp')
-	os.makedirs('.ecoli/tmp')
+	shutil.rmtree(get_local_dir() + '/tmp')
+	os.makedirs(get_local_dir() + '/tmp')
 	print('Cleanup...[done]')
 
-def main(args=sys.argv[1:]):
-	"""
-	The main function.
-	"""
-
+def init_config():
 	verify_datadir()
-	localdb = pickledb.load('.ecoli/data/local.db', True)
+	localdb = pickledb.load(get_local_dir() + '/data/local.db', True)
 
 	try:
 		localdb.llen('packages_installed')
@@ -189,6 +231,12 @@ def main(args=sys.argv[1:]):
 		localdb.lcreate('packages_installed')
 		localdb.lcreate('sources')
 		localdb.ladd('sources', default_url)
+
+	return localdb
+
+def main(args=sys.argv[1:]):
+	""" The main function """
+	localdb = init_config()
 
 	parser = argparse.ArgumentParser(description='Processing library manager')
 
@@ -199,7 +247,9 @@ def main(args=sys.argv[1:]):
 	group.add_argument('-s', action='store', metavar='pattern', dest='pattern', help='search for package')
 
 	parser.add_argument('--show-repos', action='store_true', dest='run_show_repos', help='list repositories')
+	parser.add_argument('--add-repo', action='append', metavar='source', dest='add_repo', help='add repositories')
 	parser.add_argument('--list', action='store_true', dest='run_list', help='show all packages')
+	parser.add_argument('--sync', action='store_true', dest='sync_repos', help='update repositories')
 	parser.add_argument('--cleanup', action='store_true', dest='run_cleanup', help='remove cached files and packages')
 	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 	parser.parse_args()
@@ -209,6 +259,7 @@ def main(args=sys.argv[1:]):
 	take_action = False
 	results = parser.parse_args()
 
+	# Package actions
 	if results.install_package:
 		take_action = True
 		install(results.install_package, localdb)
@@ -219,20 +270,28 @@ def main(args=sys.argv[1:]):
 
 	if results.run_update:
 		take_action = True
-		update_repo(localdb)
 		update_packages(localdb)
-
-	if results.run_show_repos:
-		take_action = True
-		list_sources(localdb)
 
 	if results.pattern:
 		take_action = True
 		list_repo(localdb, results.pattern.lower())
 
+	# Optional actions
+	if results.run_show_repos:
+		take_action = True
+		list_sources(localdb)
+
+	if results.add_repo:
+		take_action = True
+		add_sources(results.add_repo,localdb)
+
 	if results.run_list:
 		take_action = True
 		list_repo(localdb)
+
+	if results.sync_repos:
+		take_action = True
+		update_repo(localdb)
 
 	if results.run_cleanup:
 		take_action = True
